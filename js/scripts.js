91d3832bb60c0b4e0d47d57e08c6550f35788db3
Jeev Pangli <jspanglez@gmail.com>
2023-03-28 20:09:41 +0100

    Added an event listener function for the button

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,17 @@ menuToggler.addEventListener('click', ev => {
   menu.classList.toggle('open');
   menuToggler.classList.toggle('active');
 });
+
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", function() {
+  const textareas = document.getElementsByTagName("textarea");
+  for (let i = 0; i < textareas.length; i++) {
+    const textarea = textareas[i];
+    if (textarea.value === "") {
+      textarea.classList.add("error")
+    } else {
+      textarea.classList.remove("error");
+    }
+  }
+});
