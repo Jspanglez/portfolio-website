57d357f3afe356e3cc1ca4ede71edba85c0ee810
Jeev Pangli <jspanglez@gmail.com>
2023-04-06 16:43:33 +0100

    Made a variable to store one colour per page

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,26 @@ menuToggler.addEventListener('click', ev => {
   menu.classList.toggle('open');
   menuToggler.classList.toggle('active');
 });
+
+let color;
+if (window.location.pathname === '/index.html') {
+  color = 'darkred';
+} 
+
+else if (window.location.pathname.includes('/page2.html')) {
+  color = '#0649db';
+} 
+
+else if (window.location.pathname.includes('/page3.html')) {
+  color = 'indigo';
+} 
+
+else if (window.location.pathname.includes('/page4.html')) {
+  color = 'green';
+} 
+
+else {
+  color = 'black';
+}
+
+document.documentElement.style.setProperty('--myColour', color);
