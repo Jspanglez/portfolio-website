b2270551950e4c8299f37e870872e07f34bc91e2
Jeev Pangli <jspanglez@gmail.com>
2023-04-16 18:23:47 +0100

    Made the event listeners arrow functions Made the alert only happen if all fields are filled in

--- js/form.js
+++ js/form.js
@@ -1,24 +1,28 @@
 const submit = document.getElementById("submit");
 
-submit.addEventListener("click", function() {
+submit.addEventListener("click", ev => {
   const textareas = document.getElementsByTagName("textarea");
+  let allFieldsNotEmpty = true;
   for (let i = 0; i < textareas.length; i++) {
     const textarea = textareas[i];
     if (textarea.value === "") {
       textarea.classList.add("error");
+      allFieldsNotEmpty = false;
     } else {
       textarea.classList.remove("error");
     }
   }
 
-  setTimeout(function() {
-    alert("Thank you! Your message has been sent!");
+  setTimeout(() => {
+    if (allFieldsNotEmpty) {
+      alert("Thank you! Your message has been sent!");
+    }
   }, 100);
 });
 
 const form = document.getElementById('myForm');
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', e => {
   e.preventDefault(); // Prevent the page from refreshing
 
   const name = document.getElementById('myName').value;
